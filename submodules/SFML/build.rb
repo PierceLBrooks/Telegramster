b3b90bd04b63abad085e715b5ac97c56073f28cf
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2019-05-05 17:37:20 -0400

    changes to sfml building

--- submodules/SFML/build.rb
+++ submodules/SFML/build.rb
@@ -4,6 +4,8 @@
 require 'rubygems'
 require 'fileutils'
 
+api = "21"
+arch = "arm"
 stl = "c++_static"
 abi = "armeabi-v7a"
 generator = ARGV[0]
@@ -18,6 +20,11 @@ end
 Dir.mkdir(build)
 
 cwd = Dir.getwd
+standalone = File.join(cwd, "standalone")
+
+command = ["python", File.join(ndk, "build", "tools", "make_standalone_toolchain.py"), "--arch", arch, "--api", api, "--install-dir", standalone]
+print(command.join(" ").to_s+"\n")
+
 Dir.chdir(build)
 
 cmake = File.join(sdk, "cmake")
@@ -27,7 +34,7 @@ cmake = File.join(cmake, "bin")
 ninja = File.join(cmake, "ninja")
 cmake = File.join(cmake, "cmake")
 
-command = ["cmake", "-G", generator, "-DCMAKE_MAKE_PROGRAM="+ninja, "-DCMAKE_SYSTEM_NAME=Android", "-DCMAKE_ANDROID_NDK="+ndk, "-DCMAKE_ANDROID_ARCH_ABI="+abi, "-DCMAKE_ANDROID_STL_TYPE="+stl, "-DCMAKE_BUILD_TYPE=Debug", "-DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang", "-DANDROID_STL="+stl, "-DANDROID_ABI="+abi, "-DANDROID_TOOLCHAIN=clang", "./../SFML"]
+command = ["cmake", "-G", generator, "-DCMAKE_SYSTEM_NAME=Android", "-DCMAKE_SYSTEM_VERSION="+api, "-DCMAKE_ANDROID_NDK="+ndk, "-DCMAKE_ANDROID_STANDALONE_TOOLCHAIN="+standalone, "-DCMAKE_ANDROID_ARCH_ABI="+abi, "-DCMAKE_ANDROID_STL_TYPE="+stl, "-DCMAKE_BUILD_TYPE=Debug", "-DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang", "-DANDROID_STL="+stl, "-DANDROID_PLATFORM=android-"+api, "-DANDROID_NATIVE_API_LEVEL="+api, "-DANDROID_ABI="+abi, "-DANDROID_TOOLCHAIN=clang", "./../SFML"]
 print(command.join(" ").to_s+"\n")
 if not (system(*command))
 	exit
